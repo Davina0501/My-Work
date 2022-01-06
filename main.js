b8866e80f66199fd6dbd15d4da5e319c866c9585
Davina <88487967+Davina0501@users.noreply.github.com>
2022-01-06 17:56:29 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,68 @@
-      
-Webcam.attach( '#camera' );
+prediction_1 = ""
 
-camera = document.getElementById("camera");
-      
-  Webcam.set({
+Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
     png_quality:90
   });
 
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+      
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
-  console.log('ml5 version:', ml5.version);
   
-  // Initialize the Image Classifier method with MobileNet
-//classifier = ml5.imageClassifier('//model.json',modelLoaded);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5wlr_qb9M/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PZw0Umlmp/model.json',modelLoaded);
 
-  // When the model is loaded
+console.log('ml5 version:', ml5.version);
+
   function modelLoaded() {
     console.log('Model Loaded!');
   }
-      
+  
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "The first prediction is " + prediction_1;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+  synth.speak(utterThis);
+}
+
+
   function check()
   {
-    img = document.getElementById('selfie_image');
+    img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
   }
 
 
-// A function to run when we get any errors and the results
 function gotResult(error, results) {
-  // Display error in the console
   if (error) {
     console.error(error);
   } else {
-    // The results are in an array ordered by confidence.
     console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    prediction_1 = results[0].label;
+    speak();
+    if(results[0].label == "fistbump")
+    {
+	    document.getElementById("update_emoji").innerHTML = "👊";
+    }
+    if(results[0].label == "victory")
+    {
+	    document.getElementById("update_emoji").innerHTML = "✌";
+    }
+    if(results[0].label == "thumbsup")
+    {
+	    document.getElementById("update_emoji").innerHTML = "👍";
+    }
   }
 }
+  
