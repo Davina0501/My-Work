4a56db95cbe4b3447c63e177c920907fc6172e19
Davina <88487967+Davina0501@users.noreply.github.com>
2022-03-03 18:21:44 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music_1="";
 music_2="";
+rightwristX=0;
+rightwristY=0;
+leftwristX=0;
+leftwristY=0;
 
 function setup(){
     canvas=createCanvas(600,500);
@@ -7,6 +11,8 @@ function setup(){
     background("pink");
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function preload(){
@@ -18,4 +24,22 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("#FF0000");
+}
+
+function modelLoaded(){
+
+    console.log('Model Loaded');
+}
+
+
+function gotPoses(results){
+
+    if(results.length > 0){
+        rightwristX=results[0].pose.rightWrist.x;
+        rightwristY=results[0].pose.rightWrist.y;
+        leftwristX=results[0].pose.leftWrist.x;
+        leftwristY=results[0].pose.leftWrist.y;
+        console.log("rightwrsitX" + rightwristX + "rigthwristY" + rightwristY);
+        console.log("leftwristX" + leftwristX + "leftwristY" + leftwristY);
+    }
 }
